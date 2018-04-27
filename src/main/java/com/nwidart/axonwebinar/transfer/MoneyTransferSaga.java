397e54198659774e2fb49911aa42d791c7089f62
Nicolas Widart <n.widart@gmail.com>
2018-04-27 20:05:25 +0200

    Adding cancel deposit command on failure

--- src/main/java/com/nwidart/axonwebinar/transfer/MoneyTransferSaga.java
+++ src/main/java/com/nwidart/axonwebinar/transfer/MoneyTransferSaga.java
@@ -5,12 +5,16 @@ import com.nwidart.axonwebinar.coreapi.account.DepositMoneyCommand;
 import com.nwidart.axonwebinar.coreapi.account.MoneyDepositedEvent;
 import com.nwidart.axonwebinar.coreapi.account.MoneyWithdrawnEvent;
 import com.nwidart.axonwebinar.coreapi.account.WithdrawMoneyCommand;
+import com.nwidart.axonwebinar.coreapi.transfer.CancelMoneyTransferCommand;
 import com.nwidart.axonwebinar.coreapi.transfer.CompleteMoneyTransferCommand;
 import com.nwidart.axonwebinar.coreapi.transfer.MoneyTransferCompletedEvent;
 import com.nwidart.axonwebinar.coreapi.transfer.MoneyTransferRequestedEvent;
+import org.axonframework.commandhandling.CommandCallback;
+import org.axonframework.commandhandling.CommandMessage;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.axonframework.eventhandling.saga.EndSaga;
 import org.axonframework.eventhandling.saga.SagaEventHandler;
+import org.axonframework.eventhandling.saga.SagaLifecycle;
 import org.axonframework.eventhandling.saga.StartSaga;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -19,14 +23,26 @@ public class MoneyTransferSaga {
   @Autowired
   private transient CommandGateway commandGateway;
   private String targetAccount;
+  private String transferId;
 
   @StartSaga
   @SagaEventHandler(associationProperty = "transferId")
   public void on(MoneyTransferRequestedEvent event) {
     targetAccount = event.getTargetAccount();
-    commandGateway
-        .send(new WithdrawMoneyCommand(event.getSourceAccount(), event.getTransferId(),
-            event.getAmount()), LoggingCallback.INSTANCE);
+    transferId = event.getTransferId();
+    SagaLifecycle.associateWith("transactionId", transferId);
+    commandGateway.send(new WithdrawMoneyCommand(event.getSourceAccount(), transferId, event.getAmount()),
+        new CommandCallback<WithdrawMoneyCommand, Object>() {
+          @Override
+          public void onSuccess(CommandMessage<? extends WithdrawMoneyCommand> commandMessage, Object result) {
+
+          }
+
+          @Override
+          public void onFailure(CommandMessage<? extends WithdrawMoneyCommand> commandMessage, Throwable cause) {
+            commandGateway.send(new CancelMoneyTransferCommand(event.getTransferId()));
+          }
+        });
   }
 
   @SagaEventHandler(associationProperty = "transactionId")
